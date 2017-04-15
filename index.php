b461ce605134322bf2944691f25137ccbacc3156
Daniel Jones <daniel.jones@satago.co.uk>
2017-04-15 11:47:06 +0100

    Added header, main and footer

--- index.php
+++ index.php
@@ -6,6 +6,19 @@
         <title>Reel2Reel</title>
     </head>
     <body>
-        <h1>Reel2Reel</h1>
+        <header>
+            <h1>Reel2Reel</h1>
+        </header>
+        <main>
+            <p>Click the button to start a new game:</p>
+        </main>
+        <footer>
+            <p>
+                <span>Reel2Reel</span> was created by Dan and Will, with the help of numerous London pubs.
+            </p>
+            <p>
+                Our thanks go to <a href="http://www.omdbapi.com/" target="_blank">OMDB</a>.
+            </p>
+        </footer>
     </body>
 </html>
