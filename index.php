4932620e5d657c8efbf59a262054d3cfea34097e
Daniel Jones <daniel.jones@satago.co.uk>
2017-04-15 11:25:26 +0100

    Use spaces instead of tabs

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<link rel="stylesheet" href="stylesheets/reset.css" />
-		<title>Reel2Reel</title>
-	</head>
-	<body>
-		<h1>Reel2Reel</h1>
-	</body>
+    <head>
+        <link rel="stylesheet" href="stylesheets/reset.css" />
+        <title>Reel2Reel</title>
+    </head>
+    <body>
+        <h1>Reel2Reel</h1>
+    </body>
 </html>
