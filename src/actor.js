14273fb77b8c33f2843d130547dcd6a9d22ef5d9
Daniel Jones <daniel.jones@satago.co.uk>
2017-04-24 22:34:54 +0100

    Added TODO

--- src/actor.js
+++ src/actor.js
@@ -3,6 +3,7 @@ var React = require('react');
 
 class Actor extends React.Component {
     render() {
+        // TODO add anchor element (and onclick event)
         return <div className="r2r-actor">
             <img className="r2r-actor-avatar" src="images/default-actor.png" />
         </div>
