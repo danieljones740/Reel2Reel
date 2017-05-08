31a1217aa1f00fa4f56fdc7138d2e52d46d970f9
Daniel Jones <daniel.jones@satago.co.uk>
2017-05-08 20:31:31 +0100

    Removed comment

--- src/actor.js
+++ src/actor.js
@@ -9,7 +9,6 @@ class Actor extends React.Component {
     }
 
     render() {
-        // TODO add anchor element (and onclick event)
         return <div className="r2r-actor">
             <a onClick={this.handleClick}>
                 <img className="r2r-actor-avatar" src={this.props.image} />
