66e6bd3d8f23eec484c3659d6abe41d7a3ad14ec
Daniel Jones <daniel.jones@satago.co.uk>
2017-05-08 22:35:17 +0100

    Added TODOs to SelectMovie component

--- src/select-movie.js
+++ src/select-movie.js
@@ -3,15 +3,21 @@ let React = require('react');
 
 class SelectMovie extends React.Component {
 
+    // TODO store options in state?
+
     render() {
 
         let contents = this.props.actor
-            ? <p>Actor selected</p>
+            ? <p>Loading movies...</p>
             : <p>Select an actor</p>;
 
         return <div className="r2r-select-movie">
             { contents }
         </div>
+    }
+
+    componentDidMount() {
+        // TODO query for movies
     }
 }
 
