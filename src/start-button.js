4584191e6264b7aa39c1f6be31fdd2d74d223ef4
Daniel Jones <daniel.jones@satago.co.uk>
2017-05-08 21:53:26 +0100

    Added class to start button

--- src/start-button.js
+++ src/start-button.js
@@ -3,7 +3,7 @@ var React = require('react');
 
 class StartButton extends React.Component {
     render() {
-        return <button onClick={this.props.handleClick}>Start a new game</button>;
+        return <button onClick={this.props.handleClick} className="r2r-btn-start">Start a new game</button>;
     }
 }
 
